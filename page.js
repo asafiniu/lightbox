931141ffd526fcc86bd446403265906b70e5e36b
Asafiniu <asafiniu@gmail.com>
2016-08-09 17:55:00 -0700

    Pull from Google API

--- page.js
+++ page.js
@@ -3,15 +3,30 @@
         alert(msg);
     };
 
-    var start = function(){
-        var old = new Lightbox();
-        var newb = new Lightbox();
-        alert(old.gettersetter("someguy"));
-        alert(newb.gettersetter("someguy2"));
+    var getImages = function(cbk){
+        var goog = new GoogleSearchEngine('006788635243508668146:f0e7orztwgs', 'AIzaSyAaZ4dqB21xxTtdB94ZLxZoI4_Xw5GzUnE');
+        goog.query({
+            query: "slack",
+            callback: function(items) {
+                var images = [];
+                items = items || [];
+                for (var i = 0; i < items.length; i++) {
+                    images.push(items[i].image);
+                }
+
+                cbk(images);
+            }
+        });
     };
 
     // Window events
     window.onload = function(e){
-        start();
+        getImages(function(images){
+            for (var i = 0; i < images.length; i++) {
+                var img = document.createElement("img");
+                img.src = images[i].thumbnailLink;
+                document.body.appendChild(img);
+            }
+        });
     };
 })();
